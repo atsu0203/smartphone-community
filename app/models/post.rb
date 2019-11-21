7896b8745cab19e4688327b40e596e3232fc4fd9
atsu0203 <atsu0203@gmail.com>
2019-11-21 19:13:31 +0900

    add-backgroundimage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,18 @@
 class Post < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
-  has_many :comments   
+  has_many :comments 
+  has_many :likes, dependent: :destroy 
+  has_many :liking_users, through: :likes, source: :user
   mount_uploader :image, ImageUploader
+  def self.search(search)
+    return Post.all unless search
+    Post.where('text LIKE(?)', "%#{search}%")
+  end
+  
+  has_many :likes, dependent: :destroy
+  def like_user(user_id)
+   likes.find_by(user_id: user_id)
+  end
 end
 
