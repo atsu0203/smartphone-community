64e512475e40fc7735dd114b58bc7ecccb56fc15
atsu0203 <atsu0203@gmail.com>
2019-12-08 22:51:27 +0900

    add-model-posts_tag

---
+++ db/migrate/20191208134840_create_posts_tags.rb
@@ -0,0 +1,10 @@
+class CreatePostsTags < ActiveRecord::Migration[5.2]
+  def change
+  create_table :post_tags do |t|
+    t.references :post, index: true, foreign_key: true
+    t.references :tag, index: true, foreign_key: true
+
+    t.timestamps null: false
+  end
+end
+
