85c04c851f28d2fd86b28d5433b19b22c39f3721
atsu0203 <atsu0203@gmail.com>
2020-01-11 22:47:31 +0900

    add-comment.js

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,11 @@
 class CommentsController < ApplicationController
   def create
     comment = Comment.create(comment_params)
-    redirect_to "/posts/#{comment.post.id}" 
+    respond_to do |format|
+      format.html { redirect_to tweet_path(params[:tweet_id])  }
+      format.json
+    end
+      # redirect_to "/posts/#{comment.post.id}" 
   end
 
   private
