ce5a1ccd0fdeb71d309a9e44ae709647c1156562
atsu0203 <atsu0203@gmail.com>
2020-01-13 23:27:28 +0900

    Update post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,9 +4,7 @@ class Post < ApplicationRecord
   belongs_to :category
   has_many :comments 
   has_many :likes, dependent: :destroy
-  def like_user(user_id)
-   likes.find_by(user_id: user_id)
-  end
+  has_many :liked_users, through: :likes, source: :user
 
   mount_uploader :image, ImageUploader
 
