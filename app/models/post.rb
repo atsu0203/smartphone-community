7a80eadaf26b4138d7d15a426984539d4f6ebd6b
atsu0203 <atsu0203@gmail.com>
2020-01-14 03:50:18 +0900

    fix css-search result

--- app/models/post.rb
+++ app/models/post.rb
@@ -21,7 +21,6 @@ class Post < ApplicationRecord
 
   def self.search(search)
     return Post.all unless search
-    Post.where('text LIKE(?)', "%#{search}%")
+    Post.where('name LIKE(?) OR text LIKE(?)', "%#{search}%","%#{search}%")
   end
 end
-
