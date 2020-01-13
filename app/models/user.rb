53ae4ed4e91e5b2df51573db5f4a995d78351242
atsu0203 <atsu0203@gmail.com>
2020-01-13 23:25:58 +0900

    user.rb-model update

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,14 +5,16 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  has_many :posts
+         :recoverable, :rememberable, :trackable,:validatable
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
   has_many :comments
 
 
 
 
-  
+
 
   devise :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
 
