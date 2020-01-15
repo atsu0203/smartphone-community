e79aad13b2e194cc68710cad028da66c14d2ece8
atsu0203 <atsu0203@gmail.com>
2020-01-15 13:42:02 +0900

    add-null:false-name,text,image

--- db/schema.rb
+++ db/schema.rb
@@ -47,9 +47,9 @@ ActiveRecord::Schema.define(version: 2020_01_11_060931) do
   end
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "text"
-    t.text "image"
+    t.string "name", null: false
+    t.string "text", null: false
+    t.text "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
