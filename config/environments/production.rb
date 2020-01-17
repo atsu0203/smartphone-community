772c71bd74f4425cd572e078473a7e978207408b
atsu0203 <atsu0203@gmail.com>
2020-01-17 21:53:57 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # 変更した
-  config.force_ssl = false
+  # config.force_ssl = true
+
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
