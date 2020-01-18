09f96fd01477ccd3b0d3102b592da05c05f922c2
atsu0203 <atsu0203@gmail.com>
2020-01-18 23:59:38 +0900

    add-email-test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,17 @@
 require 'rails_helper'
 describe User do
   describe '#create' do
+
     it "is invalid without a nickname" do
-      user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+      user = build(:user, nickname: "")
       user.valid?
       expect(user.errors[:nickname]).to include("を入力してください")
+
+    it "is invalid without an email" do
+      user = build(:user, email: "")
+      user.valid?
+      expect(user.errors[:email]).to include("を入力してください")
+    end
     end
   end
 end
