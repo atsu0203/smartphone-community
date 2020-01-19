999d90eb6bc92b2b066cb2845aae0eacafca79d9
atsu0203 <atsu0203@gmail.com>
2020-01-19 15:14:37 +0900

    fix-and-add-fix-postcontroller

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,22 +1,37 @@
 require 'rails_helper'
 
 describe PostsController, type: :controller do
+  
   describe 'GET #new' do
     it "renders the :new template" do
       user = create(:user)
+      sign_in user
       get :new
       expect(response).to render_template :new
     end
   end
+
+  describe 'GET #show' do
+    it "renders the :show template" do
+      post = create(:post)
+      get :show, params: {  id: post }
+      expect(response).to render_template :show
+    end
+  end
+
   describe 'GET #edit' do
     it "assigns the requested tweet to @post" do
+      user = create(:user)
+      sign_in user
       post = create(:post)
       get :edit, params: { id: post }
       expect(assigns(:post)).to eq post
     end
 
     it "renders the :edit template" do
-      tweet = create(:tweet)
+      user = create(:user)
+      sign_in user
+      post = create(:post)
       get :edit, params: { id: post }
       expect(response).to render_template :edit
     end
@@ -26,9 +41,12 @@ describe PostsController, type: :controller do
     it "populates an array of tweets ordered by created_at DESC" do
       posts = create_list(:post, 3) 
       get :index
+      expect(assigns(:posts)).to match(posts.sort{ |a, b| b.created_at <=> a.created_at } )
     end
 
     it "renders the :index template" do
+      get :index
+      expect(response).to render_template :index
     end 
   end
 
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     name               {"smartphone"}
     text               {"iPhone is good smartphone."}
     image              { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/test.png')) }
+    created_at         { Faker::Time.between(from: DateTime.now - 2, to: DateTime.now) }
     user
     category
   end
