1b794b935c2fa3cff456ff74146a72973573fecd
atsu0203 <atsu0203@gmail.com>
2020-01-21 12:14:57 +0900

    change-production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   # 変更あり
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
 
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
@@ -49,7 +49,7 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # 変更した
-  config.force_ssl = false
+  config.force_ssl = true
 
 
   # Use the lowest log level to ensure availability of diagnostic information
