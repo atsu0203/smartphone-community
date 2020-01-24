657b959e6423b4717b187be86444d0dc698d406a
atsu0203 <atsu0203@gmail.com>
2020-01-25 07:26:34 +0900

    fix-user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
   mount_uploader :avatar, AvatarUploader
-
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable,:validatable
@@ -11,45 +8,10 @@ class User < ApplicationRecord
 #  has_many :liked_posts, through: :likes, source: :post
   has_many :comments
 
-  validates :nickname, presence: true, length: { maximum: 6 }
+  validates :nickname, presence: true, length: { maximum: 8 }
 
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
 
-
-
-
-  devise :omniauthable,omniauth_providers: [:facebook, :google_oauth2]
-
-  def self.find_oauth(auth)
-    uid = auth.uid
-    provider = auth.provider
-    snscredential = SnsCredential.where(uid: uid, provider: provider).first
-    if snscredential.present?
-      user = User.where(id: snscredential.user_id).first
-    else
-      user = User.where(email: auth.info.email).first
-      if user.present?
-        SnsCredential.create(
-          uid: uid,
-          provider: provider,
-          user_id: user.id
-          )
-      else
-        user = User.create(
-          nickname: auth.info.name,
-          email:    auth.info.email,
-          password: Devise.friendly_token[0, 20],
-          telephone: "08000000000"
-          )
-        SnsCredential.create(
-          uid: uid,
-          provider: provider,
-          user_id: user.id
-          )
-      end
-    end
-    return user
-  end
 end
