7314391c0465dafb36bd1178ea81513a396b6e20
atsu0203 <atsu0203@gmail.com>
2020-02-02 22:24:39 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,6 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @nickname = @user.nickname
     @posts = @user.posts.page(params[:page]).per(6).order("created_at DESC")
-    @ture_user_msg = Group.find_by(name: current_user.nickname, id: GroupUser.where(user_id: @user.id).pluck(:group_id))
+    @ture_user_msg = Group.find_by(name: @user.nickname, id: GroupUser.where(user_id: @user.id).pluck(:group_id))
   end
 end
