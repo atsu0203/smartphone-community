0819de836659a20b960370f092d16ad6e823643f
atsu0203 <atsu0203@gmail.com>
2020-02-10 19:16:16 +0900

    Update messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -26,5 +26,6 @@ class MessagesController < ApplicationController
   def set_group
     @group = Group.find(params[:group_id])
     @groupname = @group.name
+    @groupusers = @group.users
   end
 end
